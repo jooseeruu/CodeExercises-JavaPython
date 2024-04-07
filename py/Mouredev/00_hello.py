0941583f3821cde315f314e7856dc43203ac4288
jooseeruu <arjona2908@gmail.com>
2024-04-07 20:44:36 +0200

    codewars + curso brais

---
+++ py/Mouredev/00_hello.py
@@ -0,0 +1,7 @@
+# Cómo consultar el tipo de dato
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
