97fd2ec8e868d26a150ce274edeb985ebe807543
jooseeruu <arjona2908@gmail.com>
2024-05-16 21:59:37 +0200

    update

---
+++ java/sintaxis.java
@@ -0,0 +1,32 @@
+//Sitio web oficial de Java: https://www.java.com/
+
+// Sintaxis de comentarios en una línea
+/*
+Sintaxis de comentarios
+en varias líneas
+*/
+public class sintaxis{
+    public static void main(String[] args) {
+        //Tipos de dato
+        String texto = "Hola, Java!"; //cadena de texto
+        int entero = 28; // numero entero
+        boolean booleano = true; // booleano
+        double decimal = 3.14; // decimal de precision doble
+        float flotante = 5.32f; // decimal de precision simple
+        char caracter = 'A'; // caracter
+
+        //variable
+        int mivariable = 22; // variable de tipo entero con valor 22
+        //constantes
+        final double pi =3.1416;
+        String lenguaje = "java";
+
+        //Imprimir por consola
+        System.out.println("Hola "+ lenguaje);
+
+
+
+
+
+    }
+}
