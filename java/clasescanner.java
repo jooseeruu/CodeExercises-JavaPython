cb67e9ee53f8f4fb67439a74c8e6fb991d9e1857
jooseeruu <arjona2908@gmail.com>
2024-05-21 18:48:29 +0200

    clase scanner

---
+++ java/clasescanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class clasescanner {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce tu nombre");
+        String nombre = entrada.nextLine();
+        System.out.println("Hola, " + nombre);
+    }
+}
+
