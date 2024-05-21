d8051e8bd7a695bb49c6b20139bb1bdf38e7fccd
jooseeruu <arjona2908@gmail.com>
2024-05-21 18:38:37 +0200

    update

--- java/string_metodos.java
+++ java/string_metodos.java
@@ -6,5 +6,24 @@ public class string_metodos {
             System.out.println(mi_nombre.charAt(i));
         }
         System.out.println("Mi nombre tiene "+longitud+" letras");
+        String mi_nombre_resumido= mi_nombre.substring(6,9);
+        System.out.println(mi_nombre_resumido);
+
+        String nombre1, nombre2;
+        nombre1="albiño";
+        nombre2="Albiño"; 
+        boolean check=nombre1.equals(nombre2);
+        boolean check_2=nombre1.equalsIgnoreCase(nombre2);
+        System.err.println(check_2);
+        String check_definitivo;
+        if (check == true) {
+            check_definitivo=Boolean.toString(check);
+            check_definitivo=check_definitivo + "!";
+        } else {
+            check_definitivo=Boolean.toString(check);
+            check_definitivo=check_definitivo + ":(";
+        }
+
+        System.out.println(check_definitivo);
     }
 }
