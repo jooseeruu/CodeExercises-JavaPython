3dc0b1d0d0e74cfd27b73782d662f1fa562b8ab6
jooseeruu <arjona2908@gmail.com>
2024-05-21 19:55:45 +0200

    update

--- java/clasescanner.java
+++ java/clasescanner.java
@@ -5,7 +5,10 @@ public class clasescanner {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce tu nombre");
         String nombre = entrada.nextLine();
-        System.out.println("Hola, " + nombre);
+        System.out.println("Introduce tu edad");
+        int edad=entrada.nextInt();
+        System.out.println("Hola, " + nombre +" estas ya mayor "+ edad +" jubilate");
+        
     }
 }
 
---
+++ java/evaluoedad.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class evaluoedad {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        System.err.println("Introduce tu edad");
+        int edad=entrada.nextInt();
+        if (edad>=18) {
+            System.out.println("Eres grandote");
+        }
+        else if(edad>=0 && edad<=10){
+            System.out.println("vas al cole");
+        }
+        else{
+            System.out.println("es imposible que seas menor de 0 años myg");
+        }
+    }
+}
