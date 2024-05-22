25656d6c77cf32e1a4ef8dfba95e75c03885d8a5
jooseeruu <arjona2908@gmail.com>
2024-05-22 20:53:52 +0200

    Update

--- java/peso_ideal.java
+++ java/peso_ideal.java
@@ -20,9 +20,9 @@ public class peso_ideal {
             double peso_ideal_mujer=0.675*altura-56.25;
             System.out.println("Tu peso ideal sería: "+peso_ideal_mujer);
             if(peso_ideal_mujer<peso){
-                System.out.println("Ponte las pilas te sobran kilos, pesas"+peso+"kg"+"y tu peso ideal sería"+peso_ideal_mujer+"kg"+"tienes que bajar"+(peso-peso_ideal_mujer));
+                System.out.println("Ponte las pilas te sobran kilos, pesas "+peso+"kg"+"y tu peso ideal sería"+peso_ideal_mujer+"kg"+"tienes que bajar"+(peso-peso_ideal_mujer));
             }else{
-            System.out.println("Ponte las pilas hay que comer más, pesas"+peso+"kg"+"y tu peso ideal sería"+peso_ideal_mujer+"kg"+"tienes que subir"+(peso_ideal_mujer-peso));}
+            System.out.println("Ponte las pilas hay que comer más, pesas "+peso+"kg"+"y tu peso ideal sería"+peso_ideal_mujer+"kg"+"tienes que subir"+(peso_ideal_mujer-peso));}
         }
         else if(genero.equalsIgnoreCase("H")){
             System.out.println("Introduce tu altura en cm");
@@ -31,9 +31,9 @@ public class peso_ideal {
             peso=entrada.nextDouble();
             double peso_ideal_hombre=0.75*altura-62.5;
             if(peso_ideal_hombre<peso){
-                System.out.println("Ponte las pilas te sobran kilos, pesas"+peso+"kg"+"y tu peso ideal sería"+peso_ideal_hombre+"kg"+"tienes que bajar"+(peso-peso_ideal_hombre));
+                System.out.println("Ponte las pilas te sobran kilos, pesas "+peso+"kg"+"y tu peso ideal sería"+peso_ideal_hombre+"kg"+"tienes que bajar"+(peso-peso_ideal_hombre));
             }else{
-            System.out.println("Ponte las pilas hay que comer más, pesas"+peso+"kg"+"y tu peso ideal sería"+peso_ideal_hombre+"kg"+"tienes que subir"+(peso_ideal_hombre-peso));}
+            System.out.println("Ponte las pilas hay que comer más, pesas "+peso+"kg"+"y tu peso ideal sería"+peso_ideal_hombre+"kg"+"tienes que subir"+(peso_ideal_hombre-peso));}
         }
     }
 }
