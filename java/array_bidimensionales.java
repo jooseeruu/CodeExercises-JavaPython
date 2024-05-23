7aa22bf6977549b6117eb6aa52b1517bf4ca64dc
jooseeruu <arjona2908@gmail.com>
2024-05-23 20:12:17 +0200

    update

---
+++ java/array_bidimensionales.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class array_bidimensionales {
+    public static void main(String[] args) {
+        int [][] matrix=new int [4][5];
+        Random rand = new Random();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = rand.nextInt(100);
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
