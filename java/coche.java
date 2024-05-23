db5057a195fc3e76fe71131ecb62e8b7c6fa78b4
jooseeruu <arjona2908@gmail.com>
2024-05-23 21:16:18 +0200

    update

---
+++ java/coche.java
@@ -0,0 +1,14 @@
+public class coche {
+    private int ruedas;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int peso;
+    public coche(){ //CONSTRUCTOR CLASE COCHE
+        ruedas=4;
+        largo=2000;
+        ancho=300;
+        motor=1800;
+        peso=500;
+    }
+}
---
+++ java/uso_coche.java
@@ -0,0 +1,7 @@
+public class uso_coche {
+        public static void main(String[] args) {
+            coche Renault=new coche(); //INSTANCIAR UNA CLASE O EJEMPLARIZAR CLASE
+            System.out.println("Este coche tiene "+Renault.ruedas+ " ruedas");
+        }
+    }
+
