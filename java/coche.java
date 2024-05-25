87dc82cd2e695e2a64f8cc31dd65566c89ce8f30
jooseeruu <arjona2908@gmail.com>
2024-05-25 18:32:32 +0200

    update

--- java/coche.java
+++ java/coche.java
@@ -4,6 +4,35 @@ public class coche {
     private int ancho;
     private int motor;
     private int peso;
+    String color;
+    int peso_total;
+    boolean asientos_cuero,climatizador;
+
+
+    public int dime_largo(){ //GETTERS devuelve el valor de las propiedades de los objetos
+        return largo;
+    }
+    public int dime_ruedas(){
+        return ruedas;
+    }
+    public int dime_ancho(){
+        return ancho;
+    }
+    public int dime_motor(){
+        return motor;
+    }
+    public int dime_peso(){
+        return peso;
+    }
+    public String dime_color(){
+        return color;
+    }
+
+    public void establecer_color(){ //SETTERS modifica  el valor de las propiedades de los objetos
+        color="azul";
+    }
+
+
     public coche(){ //CONSTRUCTOR CLASE COCHE
         ruedas=4;
         largo=2000;
--- java/uso_coche.java
+++ java/uso_coche.java
@@ -1,7 +1,10 @@
 public class uso_coche {
         public static void main(String[] args) {
             coche Renault=new coche(); //INSTANCIAR UNA CLASE O EJEMPLARIZAR CLASE
-            System.out.println("Este coche tiene "+Renault.ruedas+ " ruedas");
+            Renault.establecer_color();
+            System.out.println(Renault.dime_color());
+            System.out.println("Este coche tiene "+Renault.dime_largo()+ " ruedas");
+    
         }
     }
 
