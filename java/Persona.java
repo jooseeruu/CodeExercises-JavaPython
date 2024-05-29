e607ba76786d1d7913f1b8aaf69314fe321b5c2a
jooseeruu <arjona2908@gmail.com>
2024-05-29 19:00:30 +0200

    update

---
+++ java/Persona.java
@@ -0,0 +1,128 @@
+/*Sus atributos son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura. No queremos que se accedan directamente a ellos. Piensa que modificador de acceso es el más adecuado, también su tipo. Si quieres añadir algún atributo puedes hacerlo.
+Por defecto, todos los atributos menos el DNI serán valores por defecto según su tipo (0 números, cadena vacía para String, etc.). Sexo sera hombre por defecto, usa una constante para ello.
+Se implantaran varios constructores:
+Un constructor por defecto.
+Un constructor con el nombre, edad y sexo, el resto por defecto.
+Un constructor con todos los atributos como parámetro.
+Los métodos que se implementaran son:
+calcularIMC(): calculara si la persona esta en su peso ideal (peso en kg/(altura^2  en m)), si esta fórmula devuelve un valor menor que 20, la función devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), significa que esta por debajo de su peso ideal la función devuelve un 0  y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la función devuelve un 1. Te recomiendo que uses constantes para devolver estos valores.
+esMayorDeEdad(): indica si es mayor de edad, devuelve un booleano.
+comprobarSexo(char sexo): comprueba que el sexo introducido es correcto. Si no es correcto, sera H. No sera visible al exterior.
+toString(): devuelve toda la información del objeto.
+generaDNI(): genera un número aleatorio de 8 cifras, genera a partir de este su número su letra correspondiente. Este método sera invocado cuando se construya el objeto. Puedes dividir el método para que te sea más fácil. No será visible al exterior.
+Métodos set de cada parámetro, excepto de DNI.*/
+
+import java.util.Random;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String DNI;
+    private char sexo;
+    private double peso;
+    private double altura;
+
+    private static final char SEXO_DEF = 'H';
+
+    public Persona() {
+        this("", 0, SEXO_DEF, 0, 0);
+    }
+
+    public Persona(String nombre, int edad, char sexo) {
+        this(nombre, edad, sexo, 0, 0);
+    }
+
+    public Persona(String nombre, int edad, char sexo, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.DNI = generaDNI();
+    }
+
+    public int calcularIMC() {
+        double imc = peso / Math.pow(altura, 2);
+        if (imc < 20) {
+            return -1;
+        } else if (imc >= 20 && imc <= 25) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+
+    private void comprobarSexo(char sexo) {
+        if (sexo != 'H' && sexo != 'M') {
+            this.sexo = SEXO_DEF;
+        }
+    }
+
+
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", DNI='" + DNI + '\'' +
+                ", sexo=" + sexo +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+
+    private String generaDNI() {
+        int numeroDNI = new Random().nextInt(100000000);
+        char letraDNI = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(numeroDNI % 23);
+        return String.format("%08d%c", numeroDNI, letraDNI);
+    }
+
+    // Métodos set
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    // Métodos get
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+}
--- java/coche.java
+++ java/coche.java
@@ -8,7 +8,15 @@ public class coche {
     private int peso_total;
     private boolean asientos_cuero,climatizador;
 
-
+    public String dime_datos_generales() {
+        return "Coche con " + ruedas + " ruedas, de " + largo + " cm de largo y " + ancho + " cm de ancho. " +
+               "El motor tiene una potencia de " + motor + " cc y el coche pesa " + peso + " kg. " +
+               "El color del coche es " + color + ". " +
+               (asientos_cuero ? "Tiene asientos de cuero. " : "Tiene asientos normales. ") +
+               (climatizador ? "Tiene climatizador. " : "Tiene aire acondicionado. ") +
+               "El peso total es " + peso_total + " kg.";
+    }
+    
     public int dime_largo(){ //GETTERS devuelve el valor de las propiedades de los objetos
         return largo;
     }
@@ -27,8 +35,36 @@ public class coche {
     public String dime_color(){
         return color;
     }
+    public String dime_asientosCuero(){
+        if (asientos_cuero==true){
+            return "El coche lleva asientos de cuero";
+        }else{
+            return "El coche lleva asientos normales";
+        }
+    }
+    public String dime_climatizador(){
+        if (climatizador==true){
+            return "El coche lleva climatizador";
+        }else{
+            return "El coche lleva aire acondicionado";
+        }
+    }
 
-    public void establecer_color(String color){ //SETTERS modifica  el valor de las propiedades de los objetos
+    public void configura_climatizador(String climatizador){//SETTERS modifica  el valor de las propiedades de los objetos
+        if(climatizador=="si"){
+            this.climatizador=true;
+        }else{
+            this.climatizador=false;
+        }
+    }
+    public void configura_asientosCuero(String asientos_cuero){
+        if(asientos_cuero=="si"){
+            this.asientos_cuero=true;
+        }else{
+            this.asientos_cuero=false;
+        }
+    }
+    public void establecer_color(String color){ 
         this.color=color;
     }
 
